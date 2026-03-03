450c1141e4831288b3dc117487f9afc67c7908bd
pranjul-07 <katiyarpranjul8840@gmail.com>
2026-03-03 23:56:44 +0530

    initial commit

--- Recursion/day5.cpp
+++ Recursion/day5.cpp
@@ -38,6 +38,40 @@ using namespace std;
 //     return 0;
 // }
 
+// void print(int arr[],int size){
+//     cout<<"Array is " << size << endl;
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+// bool isfound(int arr[],int size,int key){
+//     print(arr,size);
+  
+//     //base case
+//     if(size==0){
+//         return false;
+//     }
+//     if(arr[0]==key){
+//         return true;
+//     }
+//     int ans=isfound(arr+1,size-1,key);
+//     return ans;
+// }
+
+// int main(){
+//     int arr[]={1,2,3,4,5};
+//     int key=3;
+//     int size=5;
+//     bool ans=isfound(arr,size,key);
+//     if(ans){
+//         cout<<"Key is found"<<endl; 
+//     }
+//     else{
+//         cout<<"Key is not found"<<endl;
+//     }
+//     return 0;
+// }
 void print(int arr[],int size){
     cout<<"Array is " << size << endl;
     for(int i=0;i<size;i++){
@@ -45,25 +79,34 @@ void print(int arr[],int size){
     }
     cout<<endl;
 }
-bool isfound(int arr[],int size,int key){
-    print(arr,size);
-  
+
+bool binarySearch(int *arr,int s,int e,int k){
+
+    print(arr,e-s+1);
+    cout<<"Start "<<s<<" End "<<e<<" Key "<<k<<endl;
     //base case
-    if(size==0){
+    if(s>e){
         return false;
+
     }
-    if(arr[0]==key){
+    int mid=s+(e-s)/2;
+
+    if(arr[mid]==e){
         return true;
     }
-    int ans=isfound(arr+1,size-1,key);
-    return ans;
+    if(arr[mid]<e){
+        return binarySearch(arr,mid+1,e,k);
+    }
+    else{
+        return binarySearch(arr,s,mid-1,k);
+    }
 }
 
 int main(){
-    int arr[]={1,2,3,4,5};
-    int key=3;
-    int size=5;
-    bool ans=isfound(arr,size,key);
+    int arr[]={1,2,3,4,5,6,7,8,9};
+    int size=9;
+    int key=5;
+    bool ans=binarySearch(arr,0,size-1,key);
     if(ans){
         cout<<"Key is found"<<endl; 
     }
@@ -72,3 +115,6 @@ int main(){
     }
     return 0;
 }
+
+
+
